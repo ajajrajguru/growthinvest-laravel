9cda86451144184b87cdd026b411326673d7dfef
Prajay Verenkar <prajay@ajency.in>
2018-02-06 15:33:13 +0530

    user links

--- resources/views/backoffice/user/users.blade.php
+++ resources/views/backoffice/user/users.blade.php
@@ -83,15 +83,16 @@
                                 <td><b><a href="{{ url('backoffice/user/'.$user->gi_code.'/step-one')}}">{{  title_case($user->first_name.' '.$user->last_name) }}</a></b></td>
                                 <td class="toggle-mob">{{  $user->email }}</td>
                                 <td class="toggle-mob">{{ title_case($user->roles()->pluck('display_name')->implode(' ')) }} </td>
-                                <td>@if(!empty($user->firm)) <a href="" target="_blank">{{  $user->firm->name  }}</a> @endif</td>
+                                <td>@if(!empty($user->firm)) <a href="{{ url('backoffice/firms/'.$user->firm->gi_code.'/')}}" target="_blank">{{  $user->firm->name  }}</a> @endif</td>
                                 <td class="toggle-mob">
-                                    <select data-id="78523" class="firm_actions form-control form-control-sm" edit-url="{{ url('backoffice/user/'.$user->gi_code.'/step-one')}}">
+                                    <!-- <select data-id="78523" class="firm_actions form-control form-control-sm" edit-url="{{ url('backoffice/user/'.$user->gi_code.'/step-one')}}">
                                     <option>--select--</option>
                                     <option value="edit-intermediary">Edit Profile</option>
-                                    </select>
+                                    </select> -->
+                                    <a href="{{ url('backoffice/user/'.$user->gi_code.'/step-one')}}">View Profile</a>
                                 </td>
 
-                            </tr>
+                            </tr> 
                         @endforeach
                     </tbody>
                 </table>
