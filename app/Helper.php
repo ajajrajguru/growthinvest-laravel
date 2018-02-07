47f80fe8464dbbcd7c293699f3f1ea8dd48767e6
Parag <parag@ajency.in>
2018-02-07 14:31:37 +0530

    cdn

--- app/Helper.php
+++ app/Helper.php
@@ -620,12 +620,10 @@ function getObjectComments($objectType, $objectId, $parent)
 
 }
 
-
-
 /**
  * Store logged in user menu in session
  */
-/
+
 function storeUserMenus($user)
 {
 
@@ -681,3 +679,53 @@ function getUserAdminMenus($user_permissions)
 
     return $menus;
 }
+
+/* USER MENU END*/
+
+/* CDN START */
+function cdn($asset)
+{
+
+    //Check if we added cdn's to the config file
+
+    if (!Config::get('app.cdn')) {
+        return asset($asset);
+    }
+
+    //Get file name & cdn's
+
+    $cdns = Config::get('app.cdn');
+
+    $assetName = basename($asset);
+
+    //remove any query string for matching
+
+    $assetName = explode("?", $assetName);
+
+    $assetName = $assetName[0];
+
+    //Find the correct cdn to use
+
+    foreach ($cdns as $cdn => $types) {
+
+        if (preg_match('/^.*\.(' . $types . ')$/i', $assetName)) {
+            return cdnPath($cdn, $asset);
+        }
+
+    }
+
+    //If we couldnt match a cdn, use the last in the list.
+
+    end($cdns);
+
+    return cdnPath(key($cdns), $asset);
+
+}
+
+function cdnPath($cdn, $asset)
+{
+
+    return "//" . rtrim($cdn, "/") . "/" . ltrim($asset, "/");
+
+}
+/* CDN END */
