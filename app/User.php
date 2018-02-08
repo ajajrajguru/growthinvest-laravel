06836aa290b0535c07edc69471696788e994acab
Prajay Verenkar <prajay@ajency.in>
2018-02-08 12:13:53 +0530

    no message

--- app/User.php
+++ app/User.php
@@ -266,7 +266,7 @@ class User extends Authenticatable
         if (!empty($nomineeApplication)) {
             $investorFai = $nomineeApplication->chargesfinancial_advisor_details;
         } else {
-            $investorFai = $investor->userFinancialAdvisorInfo();
+            $investorFai = $this->userFinancialAdvisorInfo();
             $investorFai = (!empty($investorFai)) ? $investorFai->data_value : [];
 
         }
