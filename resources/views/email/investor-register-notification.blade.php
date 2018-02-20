34ac28ba80e5afbf48fdaf9cb65c6760ae3dbe83
Prajay Verenkar <prajay@ajency.in>
2018-02-20 11:35:27 +0530

    bug fix

--- resources/views/email/investor-register-notification.blade.php
+++ resources/views/email/investor-register-notification.blade.php
@@ -3,18 +3,19 @@
 @section('content')
 
 
-Hi {{ $name }},<br/><br/>
+Welcome {{ $name }},<br/><br/>
 
-New User account created for {{ $name }} by {{ $registeredBy }} in firm {{ $firmName }} with the role {{ $role }}.<br/><br/>
+{{ $name }} has signed up as an Investor under <b><i>{{ $firmName }}</i></b> .<br/><br/>
 
 
-<strong>Details of the User:</strong><br/><br/>
+<strong>Details of the Account:</strong><br/><br/>
 
 Name: {{ $name }}<br>
 E-mail: {{ $email }} <br>
-Firm: {{ $firmName }} <br>
-Role: {{ $role }} <br><br>
+Contact #: {{ $telephone }}<br>
+Location: {{ $address }}<br><br>
 
-Please log in to the Site to view more details.<br>
+Added by: {{ $registeredBy }}<br><br>
+Please log in to the Site to view more details.  <a href="{{ url('backoffice/investor/'.$giCode.'/investor-profile')}}">view more details</a>. On your dashboard you will find the certificate relating to your certification category. This document is downloadable and there for your reference.
 
  @endsection
