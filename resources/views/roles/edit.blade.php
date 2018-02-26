df7d094942d9400746af9078a3b82455058a59fa
Unknown <neves@ajency.in>
2018-02-26 12:15:46 +0530

    issue #251 - page styling

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -1,29 +1,45 @@
-@extends('layouts.app')
+@extends('layouts.backoffice')
 
 @section('title', '| Edit Role')
 
-@section('content')
+@section('js')
+ @parent
+<script type="text/javascript" src="{{ asset('js/backoffice.js') }}"></script>
+@endsection
+
+@section('backoffice-content')
 
 <div class='container mt-5'>
     <h3><i class='fa fa-key'></i> Edit Role: {{$role->name}}</h3>
     <hr>
 
     {{ Form::model($role, array('route' => array('roles.update', $role->id), 'method' => 'PUT')) }}
+    
+    <div class="d-sm-flex justify-content-sm-between align-items-sm-center mb-5">
+        <div class="form-group mb-0">
+            {{ Form::label('name', 'Role Name') }}
+            {{ Form::text('name', null, array('class' => 'form-control')) }}
+        </div>
 
-    <div class="form-group">
-        {{ Form::label('name', 'Role Name') }}
-        {{ Form::text('name', null, array('class' => 'form-control')) }}
+        <div>
+            {{ Form::submit('Edit', array('class' => 'btn btn-sm btn-outline-primary')) }}
+        </div>
     </div>
+    
 
-    <h5><b>Assign Permissions</b></h5>
-    @foreach ($permissions as $permission)
-
-        {{Form::checkbox('permissions[]',  $permission->id, $role->permissions ) }}
-        {{Form::label($permission->name, ucfirst($permission->name)) }}<br>
-
-    @endforeach
-    <br>
-    {{ Form::submit('Edit', array('class' => 'btn btn-primary')) }}
+    <h1 class="section-title font-weight-medium text-primary mb-0">Assign Permissions</h1>
+    <div class="row mt-3">
+        @foreach ($permissions as $permission)
+        <div class="col-sm-4">
+            <div class="form-group">
+                {{Form::checkbox('permissions[]',  $permission->id, $role->permissions ) }}
+                {{Form::label($permission->name, ucfirst($permission->name)) }}
+            </div>
+        </div>
+        @endforeach
+    </div>
+    
+    
 
     {{ Form::close() }}    
 </div>
