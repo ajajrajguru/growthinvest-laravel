5528d5341d7b8f0f23a8c58ad47cbbd09e9c3451
Unknown <neves@ajency.in>
2018-04-10 16:13:24 +0530

    activity analysis - activity group & count table styled

--- resources/views/backoffice/activity/activity-group-count.blade.php
+++ resources/views/backoffice/activity/activity-group-count.blade.php
@@ -1,30 +1,32 @@
-<table >
-    <thead>
-        <th>
-        <td>Activity Group</td>
-        <td>Count</td>
-        <td>Activity Group</td>
-        <td>Count</td>
-        </th>
-    </thead>
-    <tbody>
-        <tr>
-        @php
-        $i = 1;
-        @endphp
-        @foreach($countData as $groupName => $count)             
-            <td>{{ $groupName }}</td>
-            <td>{{ $count }} </td>  
-            @if($i % 2 == 0) 
-                </tr>
-                <tr>
-            @endif
-
+<div class="table-responsive">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+            <th>Activity Group</th>
+            <th>Count</th>
+            <th>Activity Group</th>
+            <th>Count</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
             @php
-                $i++;
-            @endphp      
-        @endforeach
-        </tr>
-    </tbody>
+            $i = 1;
+            @endphp
+            @foreach($countData as $groupName => $count)             
+                <td>{{ $groupName }}</td>
+                <td>{{ $count }} </td>  
+                @if($i % 2 == 0) 
+                    </tr>
+                    <tr>
+                @endif
 
-</table>
+                @php
+                    $i++;
+                @endphp      
+            @endforeach
+            </tr>
+        </tbody>
+
+    </table>
+</div>
