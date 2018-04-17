f72a3519d50d8bc37b4c74a2cf34528d63b445ca
nevesseq <neves@ajency.in>
2018-04-17 10:52:35 +0530

    changes in activity analysis

--- resources/views/backoffice/activity/activity-group-count.blade.php
+++ resources/views/backoffice/activity/activity-group-count.blade.php
@@ -65,3 +65,65 @@
 
     </table>
 </div>
+
+<div class="row">
+    <!-- 1st -->
+    <div class="col-sm-6 border">
+        <div class="row bg-primary text-white py-2 px-3">
+            <div class="col-9">Activity Group</div>
+            <div class="col-3">Count</div>
+        </div>
+        
+        @foreach($countData as $groupName => $count)
+        <!-- collapse -->
+        <div data-toggle="collapse" data-target="#demo" class="cursor-pointer">
+            <div class="row py-2 px-3">
+                <div class="col-9"><strong>{{ $groupName }}</strong></div>
+                <div class="col-3 text-center"><strong>{{ $count }}</strong></div>
+            </div>
+        </div>
+
+        <!-- collapsible     -->
+        <div id="demo" class="collapse px-3 bg-gray">
+            @if(isset($groupActivityCount[$groupName]))                   
+            @foreach($groupActivityCount[$groupName] as $activityName =>$activityCount)
+            <div class="row py-2">
+                <div class="col-9"><span class="filter-activity-name" activity-slug="{{ $activityName }}">{{ $activityTypeList[$activityName] }}</div>
+                <div class="col-3 text-center">{{ $activityCount }}</div>
+            </div>
+            @endforeach                    
+            @endif 
+        </div>
+        @endforeach
+    </div>
+
+    <!-- 2nd -->
+    <div class="col-sm-6 border">
+        <div class="row bg-primary text-white py-2 px-3">
+            <div class="col-9">Activity Group</div>
+            <div class="col-3">Count</div>
+        </div>
+        
+        @foreach($countData as $groupName => $count)
+        <!-- collapse -->
+        <div data-toggle="collapse" data-target="#demo2" class="cursor-pointer">
+            <div class="row py-2 px-3">
+                <div class="col-9"><strong>{{ $groupName }}</strong></div>
+                <div class="col-3 text-center"><strong>{{ $count }}</strong></div>
+            </div>
+        </div>
+
+        <!-- collapsible     -->
+        <div id="demo2" class="collapse px-3 bg-gray" >
+            @if(isset($groupActivityCount[$groupName]))                   
+            @foreach($groupActivityCount[$groupName] as $activityName =>$activityCount)
+            <div class="row py-2">
+                <div class="col-9"><span class="filter-activity-name" activity-slug="{{ $activityName }}">{{ $activityTypeList[$activityName] }}</div>
+                <div class="col-3 text-center">{{ $activityCount }}</div>
+            </div>
+            @endforeach                    
+            @endif
+        </div>
+        @endforeach
+    </div>
+</div>
